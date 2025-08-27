d340a01403655c37b9c1c19ab99f85eec6365e5f
cyole <i@cyole.me>
2025-08-27 14:03:39 +0800

    chore: add 'fast-glob' and 'vue-component-meta' dependencies in package.json and pnpm-lock.yaml

--- docs/.vitepress/plugins/vite-plugin-docs.ts
+++ docs/.vitepress/plugins/vite-plugin-docs.ts
@@ -1,9 +1,35 @@
-import type { Plugin } from 'vite'
-import { existsSync, readdirSync, readFileSync, writeFileSync } from 'node:fs'
+import type { Plugin, ResolvedConfig } from 'vite'
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import fg from 'fast-glob'
+
+function generateDemos(demosDir: string) {
+  const currentMdPath = `${demosDir}/index.md`
+
+  if (!existsSync(currentMdPath)) {
+    return
+  }
+
+  const currentMdContent = readFileSync(currentMdPath, 'utf-8')
+  const allDemos = fg.sync(`${demosDir}/*.vue`, { objectMode: true })
+
+  const allDemosContent = allDemos.map((demo) => {
+    const demoPath = `${demosDir.split('/').slice(-2).join('/')}/${demo.name}`
+    const demoContent = `<demo vue="${demoPath}" />`
+    return demoContent
+  }).join('\n')
+
+  const newMdContent = currentMdContent.replace(/<!-- DEMO -->[\s\S]*<!-- DEMO -->/, `<!-- DEMO -->\n\n${allDemosContent}\n\n<!-- DEMO -->`)
+  writeFileSync(currentMdPath, newMdContent)
+}
 
 export function DocsPlugin(): Plugin {
+  let userConfig: ResolvedConfig | undefined
+
   return {
     name: 'vite-plugin-docs',
+    configResolved(config) {
+      userConfig = config
+    },
     configureServer({ watcher }) {
       watcher.on('all', (event, path) => {
         if (event !== 'add' && event !== 'unlink')
@@ -14,25 +40,15 @@ export function DocsPlugin(): Plugin {
         }
 
         const demosDir = path.split('/').slice(0, -1).join('/')
-        const currentMdPath = `${demosDir}/index.md`
 
-        if (!existsSync(currentMdPath)) {
-          return
-        }
-
-        const currentMdContent = readFileSync(currentMdPath, 'utf-8')
-
-        const allDemos = readdirSync(demosDir, { withFileTypes: true })
-        const demoFiles = allDemos.filter(demo => demo.isFile() && demo.name.endsWith('.vue'))
-
-        const allDemosContent = demoFiles.map((demo) => {
-          const demoPath = `${demosDir.split('/').slice(-2).join('/')}/${demo.name}`
-          const demoContent = `<demo vue="${demoPath}" />`
-          return demoContent
-        }).join('\n')
-
-        const newMdContent = currentMdContent.replace(/<!-- DEMO -->[\s\S]*<!-- DEMO -->/, `<!-- DEMO -->\n\n${allDemosContent}\n\n<!-- DEMO -->`)
-        writeFileSync(currentMdPath, newMdContent)
+        generateDemos(demosDir)
+      })
+    },
+    async buildStart() {
+      const allDemosDir = fg.sync(`${userConfig?.root}/src/**/demos`, { onlyDirectories: true })
+      allDemosDir.forEach((demoDir) => {
+        if (existsSync(`${demoDir}/index.md`))
+          generateDemos(demoDir)
       })
     },
   }
